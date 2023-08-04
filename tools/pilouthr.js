da230dc326fdacea96ed9a89256fa175efc8c1e7
Héctor Masip <hmasip@polygon.technology>
2023-08-04 13:03:21 +0200

    Constraint printer working

--- tools/pilouthr.js
+++ tools/pilouthr.js
@@ -1,44 +1,44 @@
 #!/usr/bin/env node
 
 const protobuf = require('protobufjs');
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 const exp = require('constants');
-const version = require("../package").version;
+const version = require('../package').version;
 
-const argv = require("yargs")
+const argv = require('yargs')
     .version(version)
-    .usage("$0 <source.ptb>")
-    // .alias("o", "output")
-    // .alias("P", "config")
-    // .alias("v", "verbose")
+    .usage('$0 <source.ptb>')
+    // .alias('o', 'output')
+    // .alias('P', 'config')
+    // .alias('v', 'verbose')
     .argv;
 
 async function run() {
     let inputFile;
     if (argv._.length == 0) {
-        console.log("You need to specify a source file");
+        console.log('You need to specify a source file');
         process.exit(1);
     } else if (argv._.length == 1) {
         inputFile = argv._[0];
     } else {
-        console.log("Only one pilout at a time is permited");
+        console.log('Only one pilout at a time is permited');
         process.exit(1);
     }
 
     const fullFileName = path.resolve(process.cwd(), inputFile);
 
     // TBD
-    // const fileName = path.basename(fullFileName, ".pil");
+    // const fileName = path.basename(fullFileName, '.pil');
     // const outputFile =
-    //     typeof argv.output === "string" ? argv.output : fileName + ".json";
+    //     typeof argv.output === 'string' ? argv.output : fileName + '.json';
     // const config =
-    //     typeof argv.config === "string"
+    //     typeof argv.config === 'string'
     //         ? JSON.parse(fs.readFileSync(argv.config.trim()))
     //         : {};
     // if (argv.verbose) {
     //     config.verbose = true;
-    //     if (typeof config.color === "undefined") {
+    //     if (typeof config.color === 'undefined') {
     //         config.color = tty.isatty(process.stdout.fd);
     //     }
     // }
@@ -47,110 +47,81 @@ async function run() {
     const piloutEnc = fs.readFileSync(fullFileName);
     const path2proto = path.join(__dirname, '..', 'src', '/pilout.proto');
     const root = protobuf.loadSync(path2proto);
-    const PilOut = root.lookupType("PilOut");
+    const PilOut = root.lookupType('PilOut');
     const piloutDec = PilOut.toObject(PilOut.decode(piloutEnc)); // pilout -> subproofs -> airs -> constraints
-    console.log("air",piloutDec.subproofs[0].airs[0].expressions[0])
-    console.log(piloutDec.subproofs[0].airs[0].constraints)
-    console.log(piloutDec.symbols)
-
-    // Classify symbols
-    // for (let i = 0; i < piloutDec.symbols.length; i++) {
-    //     const symbol = piloutDec.symbols[i];
-    //     console.log(`=== Symbol: ${symbol.name} ===`);
-    //     console.log(symbol);
-    // }
+    // console.log('air',piloutDec.subproofs[0].airs[0].expressions[0].sub)
+    // console.log(piloutDec.subproofs[0].airs[0].constraints)
+    // console.log(piloutDec.symbols)
 
     for (let i = 0; i < piloutDec.subproofs.length; i++) {
         const subproof = piloutDec.subproofs[i];
-        console.log(`=== SubProof: ${subproof.name} ===`);
+        console.log('■', `SubProof ${subproof.name}:`);
         for (let j = 0; j < subproof.airs.length; j++) {
             const air = subproof.airs[j];
-            console.log(`=== AIR: ${air.name} ===`);
+            console.log(' '.repeat(2), '◆', `AIR ${air.name}:`);
 
             // Given a constraint, we use its expressionIdx to find the expression in a recursive way
-            // TODO: Check if the following arrays is necessary
-            let everyRow = [];
-            let firstRow = [];
-            let lastRow = [];
-            let everyFrame = [];
-            for (let k = 0; k < 1; k++) {
-                let constraint = air.constraints[k];
-                constraint = findConstraintByType(constraint);
+            const types = ['everyRow', 'firstRow', 'lastRow', 'everyFrame'];
+            let constraintOrg = {};
+            for (let k = 0; k < types.length; k++) {
+                constraintOrg[types[k]] = [];
+            }
+            for (let k = 0; k < air.constraints.length; k++) {
+                let [constraint, type] = findConstraintByType(air.constraints[k], constraintOrg);
                 const expressionIdx = constraint.expressionIdx.idx;
 
                 // For now, assume the ope is binary (lhs + rhs)
-                let constt = [null, null, null];
-                constraintConstruction(expressionIdx);
+                constraintOrg[type].push(constraintConstruction(expressionIdx) + ' === 0');
 
                 function constraintConstruction(idx) {
-                    let expression = air.expressions[idx];
-                    let type;
-                    [expression, type] = findExpressionByType(expression); // First step of constraint recursive construction
-                    console.log("exp", expression, type)
-                    const lhs = expression.lhs;
-                    const rhs = expression.rhs;
-                    constt[0] = lhs;
-                    constt[1] = type;
-                    constt[2] = rhs;
-                    console.log("constt",constt)
-
-                    if (lhs.expression !== undefined) {
-                        const idx = lhs.expression.idx;
-                        constraintConstruction(idx);
-                    }
-                    if (rhs.expression !== undefined) {
-                        const idx = rhs.expression.idx;
-                        constraintConstruction(idx);
-                    }
-
+                    let [expression, op] = findExpressionByType(air.expressions[idx]);
+                    let lhs = expression.lhs;
+                    let rhs = expression.rhs;
                     let lhsElement = findExpElementByType(piloutDec.symbols, lhs);
                     let rhsElement = findExpElementByType(piloutDec.symbols, rhs);
-                    console.log("hey3",lhsElement,rhsElement)
 
-                    return [lhs, rhs]
+                    if (lhs.expression === undefined && rhs.expression === undefined) {
+                        return '(' + lhsElement + ' ' + op + ' ' + rhsElement + ')';
+                    } else if (lhs.expression === undefined && rhs.expression !== undefined) {
+                        rhs = constraintConstruction(rhs.expression.idx);
+                        return '(' + lhsElement + ' ' + op + ' ' + rhs + ')';
+                    } else if (lhs.expression !== undefined && rhs.expression === undefined) {
+                        lhs = constraintConstruction(lhs.expression.idx);
+                        return '(' + lhs + ' ' + op + ' ' + rhsElement + ')';
+                    }
+
+                    lhs = constraintConstruction(lhs.expression.idx);
+                    rhs = constraintConstruction(rhs.expression.idx);
+
+                    return lhs + ' ' + op + ' ' + rhs;
                 }
             }
 
-            console.log(`=== Constraints: EveryRow ===`);
-            for (let k = 0; k < everyRow.length; k++) {
-                const constraint = everyRow[k];
-                console.log(`=== Constraint ${k} ===`);
-                console.log(constraint.debugLine);
-            }
-
-            console.log(`=== Constraints: FirstRow ===`);
-            for (let k = 0; k < firstRow.length; k++) {
-                const constraint = firstRow[k];
-                console.log(`=== Constraint ${k} ===`);
-                console.log(constraint.debugLine);
-            }
-
-            console.log(`=== Constraints: LastRow ===`);
-            for (let k = 0; k < lastRow.length; k++) {
-                const constraint = lastRow[k];
-                console.log(`=== Constraint ${k} ===`);
-                console.log(constraint.debugLine);
-            }
-
-            console.log(`=== Constraints: EveryFrame ===`);
-            for (let k = 0; k < everyFrame.length; k++) {
-                const constraint = everyFrame[k];
-                console.log(`=== Constraint ${k} ===`);
-                console.log(constraint.debugLine);
+            for (let k = 0; k < types.length; k++) {
+                if (constraintOrg[types[k]].length > 0) {
+                    console.log(' '.repeat(4), '▲', `Constraints ${types[k]}:`);
+                    for (let l = 0; l < constraintOrg[types[k]].length; l++) {
+                        console.log(' '.repeat(6), '•', constraintOrg[types[k]][l]);
+                    }
+                }
             }
         }
     }
 }
 
-function findConstraintByType(constraint) {
+function findConstraintByType(constraint, set) {
     if (constraint.everyRow !== undefined) {
-        return constraint.everyRow;
+        return [constraint.everyRow, 'everyRow'];
+
     } else if (constraint.firstRow !== undefined) {
-        return constraint.firstRow;
+        return [constraint.firstRow, 'firstRow'];
+
     } else if (constraint.lastRow !== undefined) {
-        return constraint.lastRow;
+        return [constraint.lastRow, 'lastRow'];
+
     } else if (constraint.everyFrame !== undefined) {
-        return constraint.everyFrame;
+        return [constraint.everyFrame, 'everyFrame'];
+
     } else {
         throw new Error(`Invalid constraint type ${constraint}`);
     }
@@ -171,20 +142,35 @@ function findExpressionByType(expression) {
 
 // TODO: Complete
 function findExpElementByType(symbols, expElement) {
-    if (expElement.witnessCol !== undefined) {
+    if (expElement.expression !== undefined) {
+        return expElement.expression;
+
+    } else if (expElement.witnessCol !== undefined) {
         const witnessCol = expElement.witnessCol;
-        const type = stringToSymbol("WITNESS_COL");
-        return findSymbolByTypeAndId(symbols, type, witnessCol.colIdx);
-        // return [witnessCol.stage, witnessCol.colIdx, witnessCol.rowOffset];
+        const type = stringToSymbol('WITNESS_COL');
+
+        const symbol = findSymbolByTypeAndId(symbols, type, witnessCol.colIdx);
+        const rowOffset = witnessCol.rowOffset;
+        if (rowOffset > 0) {
+            return (rowOffset > 1 ? `${symbol}'${-rowOffset}` : `${symbol}'`);
+        }
+        if (rowOffset < 0) {
+            return (rowOffset < -1 ? `${-rowOffset}'${symbol}` : `'${symbol}`);
+        }
+        return symbol;
 
     } else if (expElement.fixedCol !== undefined) {
         const fixedCol = expElement.fixedCol;
-        const type = stringToSymbol("FIXED_COL");
+        const type = stringToSymbol('FIXED_COL');
         return findSymbolByTypeAndId(symbols, type, fixedCol.idx);
 
     } else if (expElement.constant !== undefined) {
-        return expElement.constant.value;
+        return buf2bint(expElement.constant.value);
 
+    } else if (expElement.publicValue !== undefined) {
+        const publicValue = expElement.publicValue;
+        const type = stringToSymbol('PUBLIC_VALUE');
+        return findSymbolByTypeAndId(symbols, type, publicValue.idx);
     } else {
         throw new Error(`Invalid expression element type ${expElement}`);
     }
@@ -202,52 +188,54 @@ function findSymbolByTypeAndId(symbols, type, id) {
 
 function stringToSymbol(string) {
     switch (string) {
-        case "IM_COL":
+        case 'IM_COL':
             return 0;
-        case "FIXED_COL":
+        case 'FIXED_COL':
             return 1;
-        case "PERIODIC_COL":
+        case 'PERIODIC_COL':
             return 2;
-        case "WITNESS_COL":
+        case 'WITNESS_COL':
             return 3;
-        case "PROOF_VALUE":
+        case 'PROOF_VALUE':
             return 4;
-        case "SUBPROOF_VALUE":
+        case 'SUBPROOF_VALUE':
             return 5;
-        case "PUBLIC_VALUE":
+        case 'PUBLIC_VALUE':
             return 6;
-        case "PUBLIC_TABLE":
+        case 'PUBLIC_TABLE':
             return 7;
-        case "CHALLENGE":
+        case 'CHALLENGE':
             return 8;
         default:
             throw new Error(`Invalid string ${string}`);
     }
 }
 
-function symbolToString(symbol) {
-    switch (symbol) {
-        case 0:
-            return "IM_COL";
-        case 1:
-            return "FIXED_COL";
-        case 2:
-            return "PERIODIC_COL";
-        case 3:
-            return "WITNESS_COL";
-        case 4:
-            return "PROOF_VALUE";
-        case 5:
-            return "SUBPROOF_VALUE";
-        case 6:
-            return "PUBLIC_VALUE";
-        case 7:
-            return "PUBLIC_TABLE";
-        case 8:
-            return "CHALLENGE";
-        default:
-            throw new Error(`Invalid symbol ${symbol}`);
+function buf2bint(buf) {
+    let offset = 0;
+    let value;
+    if ((buf.length - offset) >= 8) {
+        value = 0n;
+    } else {
+        value = 0;
     }
+    while ((buf.length - offset) >= 8) {
+        value = (value << 64n) + (offset ? buf.readBigUInt64BE(offset):buf.readBigInt64BE(offset));
+        offset += 8;
+    }
+    while ((buf.length - offset) >= 4) {
+        value = (value << 32) + (offset ? buf.readUInt32BE(offset):buf.readInt32BE(offset));
+        offset += 4;
+    }
+    while ((buf.length - offset) >= 2) {
+        value = (value << 16) + (offset ? buf.readUInt16BE(offset):buf.readInt16BE(offset));
+        offset += 2;
+    }
+    while ((buf.length - offset) >= 1) {
+        value += (value << 8) + (offset ? buf.readUInt8(offset):buf.readInt8(offset));
+        offset += 1;
+    }
+    return value;
 }
 
 // const originalMethod = console.log;
@@ -257,9 +245,9 @@ function symbolToString(symbol) {
 //     try {
 //         throw new Error();
 //     } catch (e) {
-//         if (typeof e.stack === "string") {
+//         if (typeof e.stack === 'string') {
 //             let isFirst = true;
-//             for (const line of e.stack.split("\n")) {
+//             for (const line of e.stack.split('\n')) {
 //                 const matches = line.match(
 //                     /^\s+at\s+.*\/([^\/:]*:[0-9]+:[0-9]+)\)/
 //                 );
@@ -279,13 +267,13 @@ function symbolToString(symbol) {
 //         originalMethod.apply(console, args);
 //     } else {
 //         initiator = initiator
-//             .split(":")
+//             .split(':')
 //             .slice(0, 2)
-//             .join(":")
-//             .replace(".js", "");
+//             .join(':')
+//             .replace('.js', '');
 //         initiator =
 //             initiator.length > maxSourceRefLen
-//                 ? "..." + initiator.substring(-maxSourceRefLen + 3)
+//                 ? '...' + initiator.substring(-maxSourceRefLen + 3)
 //                 : initiator.padEnd(maxSourceRefLen);
 //         originalMethod.apply(console, [
 //             `\x1B[30;104m${initiator} \x1B[0m`,
